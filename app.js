2ab0c76fd5248b74c1e021d4a3ebe307e7f31f66
Kyle Smith <kylesmith6568@gmail.com>
2012-07-22 22:01:45 -0400

    Testing again

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function( request, response ){
 
 // Create a SUPER SIMPLE response.
 response.writeHead( 200, {"content-type": "text/plain"} );
-response.write( "Hello world from AWS UNBUNTU!!!!!!" + Date.now() + "\nDeployed From GitHub !!!!" );
+response.write( "Deployed from Github. Suck it #azure " + Date.now());
 response.end();
 
 }
