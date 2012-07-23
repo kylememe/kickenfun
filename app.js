a58147133a57d56b960d420642fe31b7dc726ca4
Kyle Smith <kylesmith6568@gmail.com>
2012-07-22 20:50:58 -0400

    Added app.js, removed kickenfun.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var sys = require( "util" );
+var http = require( "http" );
+
+// Create our HTTP server.
+var server = http.createServer(
+function( request, response ){
+
+
+// Create a SUPER SIMPLE response.
+response.writeHead( 200, {"content-type": "text/plain"} );
+response.write( "Hello world from AWS UNBUNTU!!!!!!" + Date.now() + "\nDeployed From GitHub !!!!" );
+response.end();
+
+}
+);
+
+// Point the HTTP server to port 8080.
+server.listen( 8080 );
+
+// For logging....
+sys.puts( "Server is running on 8080" );
